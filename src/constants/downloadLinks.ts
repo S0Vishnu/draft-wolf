2efd227b04a13fcb92a365658caf6f0705394137
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2026-02-04 23:28:56 +0530

    feat: Add download links for Windows, Linux, and Mac.

--- src/constants/downloadLinks.ts
+++ src/constants/downloadLinks.ts
@@ -1,3 +1,3 @@
-export const WINDOWS_DOWNLOAD_LINK = 'https://github.com/S0Vishnu/Draftwolf/releases/download/draftwolf1.1.7/DraftWolf.1.1.7.exe';
+export const WINDOWS_DOWNLOAD_LINK = 'https://github.com/S0Vishnu/Draftwolf/releases/download/draftwolf1.1.8/DraftWolf.1.1.8.exe';
 export const LINUX_DOWNLOAD_LINK = '';
 export const MAC_DOWNLOAD_LINK = '';
