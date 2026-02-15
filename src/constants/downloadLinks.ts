3f21cc6a163d4f2e67b472bf5c6fb34a21c018ec
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2026-02-15 19:21:15 +0530

    Update Windows download link to version 1.1.10

--- src/constants/downloadLinks.ts
+++ src/constants/downloadLinks.ts
@@ -1,4 +1,5 @@
-export const WINDOWS_DOWNLOAD_LINK = 'https://github.com/S0Vishnu/Draftwolf/releases/download/draftwolf1.1.9/DraftWolf.1.1.9.exe';
+export const WINDOWS_DOWNLOAD_LINK = 'https://github.com/S0Vishnu/Draftwolf/releases/download/draftwolf1.1.10/DraftWolf.1.1.10.exe';
 export const LINUX_DOWNLOAD_LINK = '';
 export const MAC_DOWNLOAD_LINK = '';
+
 
